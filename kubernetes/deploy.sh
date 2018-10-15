f28d808b8123d9a567cfe94b1f3753688b01e03d
gruggiero <giovanni.ruggiero@gmail.com>
2018-10-15 13:31:42 +0200

    Fixed deploy

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -4,5 +4,5 @@ set -e
 
 kubectl --kubeconfig=$KUBECONFIG delete configmap catalog-manager-conf -n testci || true
 kubectl --kubeconfig=$KUBECONFIG create configmap catalog-manager-conf -n testci --from-file=../conf/${DEPLOY_ENV}/prodBase.conf
-kubectl --kubeconfig=$KUBECONFIG replace -f catalog-manager-logback.yml --force
+kubectl --kubeconfig=$KUBECONFIG replace -f catalog-manager-logback.yml -n testci --force
 kubectl --kubeconfig=$KUBECONFIG replace -f daf_catalog_manager_${DEPLOY_ENV}.yml -n testci --force
