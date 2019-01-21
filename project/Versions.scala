e28257354e220a3447307f0ed5a61b5bfe0bed96
luca-pic <luca.xxvi@gmai.com>
2019-01-21 12:04:49 +0100

    upgrade version

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
-  lazy val defaultVersion = "2.0.19"
+  lazy val defaultVersion = "2.0.19-SNAPSHOT"
 
   val playVersion = "2.5.14"
 
   val swaggerUiVersion = "3.0.10"
 
-  val dafCommonVersion = "1.1.3"
+  val dafCommonVersion = "1.1.3-SNAPSHOT"
 }
